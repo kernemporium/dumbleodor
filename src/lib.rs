c699d5f9719eafeba079131f0e2f3ebad6f816bf
n4sm <hobbyste.sda@gmail.com>
2021-01-10 20:28:57 +0100

    delete creds

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,6 @@ mod utils;
 pub trait Binary {
     fn run_binary(&mut self, argc: u64, argv: Vec<CString>, if_debug: bool) -> error::Result<()>;
     fn run(&self) -> error::Result<()>;
-
-    fn credits() {
-        println!("[** -=-=-=-=-=- ## Developed by nasm - RE ## -=-=-=-=-=-=- **]");
-    }
 }
 
 
